891cdd78c8522e17c4e77cc043e8938e769e3b21
Mahathir <mirmahathir1@gmail.com>
2021-07-17 01:55:32 +0600

    Added safeguard to make sure that frontend errors are safely shown by their name, message and stack

--- src/components/DevConsole/FrontendErrors.vue
+++ src/components/DevConsole/FrontendErrors.vue
@@ -3,13 +3,13 @@
     <v-card-title>
       {{ error.name }}
     </v-card-title>
-    <v-card-subtitle>
+    <v-card-subtitle v-if="error.time">
       {{ error.message }}. Occured at {{new Date(error.time).toLocaleString()}}
     </v-card-subtitle>
     <v-card-actions>
       <v-btn rounded depressed x-small @click="showStack = !showStack">Toggle Stack</v-btn>
     </v-card-actions>
-    <v-card-text v-if="showStack">
+    <v-card-text v-if="showStack && error.stack">
       {{error.stack}}
     </v-card-text>
   </v-card>
