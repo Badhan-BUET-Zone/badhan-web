d72f3ce4734905533f1ae58e48cd91bfa3497e45
Mahathir <mirmahathir1@gmail.com>
2021-08-22 15:29:34 +0600

    Added primary color to dark theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -15,6 +15,15 @@ export default new Vuetify({
                 error: colors.purple,
                 warning: colors.orange.lighten3,
                 success: colors.green.lighten4,
+            },
+            dark: {
+                primary: colors.red.darken3,
+                secondary: colors.pink.darken3,
+                accent: colors.red.lighten3,
+                tertiary: colors.red.lighten4,
+                error: colors.purple,
+                warning: colors.orange.lighten3,
+                success: colors.green.lighten4,
             }
         },
     },
