70fe1763cc3374335e30f629ed2b1f424f90c1ee
Mahathir <mirmahathir1@gmail.com>
2021-10-24 20:57:10 +0600

    added test calender in person creation

--- src/components/BatchInsertion/NewPersonCard.vue
+++ src/components/BatchInsertion/NewPersonCard.vue
@@ -100,9 +100,51 @@
           <v-btn small rounded text color="primary" @click="$refs.lastDonationMenu.save(lastDonation)">OK</v-btn>
         </v-date-picker>
       </v-menu>
+
+      <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="date"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+              v-model="date"
+              label="Picker in menu"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+            v-model="date"
+            no-title
+            scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+              text
+              color="primary"
+              @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+              text
+              color="primary"
+              @click="$refs.menu.save(date)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
     </v-card-text>
     <v-card-actions>
-      <v-btn small color="secondary" rounded @click="discardClicked">
+      <v-btn small color="primary" rounded @click="discardClicked">
         <v-icon left>
           mdi-delete
         </v-icon>
@@ -327,7 +369,11 @@ export default {
       departmentListShown: false,
       timeout: null,
       phoneDuplicateCheckLoader: false,
-      duplicateDonorMessage: ""
+      duplicateDonorMessage: "",
+
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      menu: false,
+      modal: false,
     }
   },
 
