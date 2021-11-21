a4f42f337f5927f697a7b5712a83c1e55a86ac6c
Mir Mahathir Mohammad <mirmahathir1@gmail.com>
2021-11-21 18:27:35 +0600

    added star beside the top three volunteers in members page

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -4,97 +4,99 @@
     </PageTitle>
     <Container>
       <transition name="slide-fade-down" mode="out-in">
-      <div v-if="getMemberLoaderFlag" :key="'membersLoading'">
-        <v-card-title>
-          Loading Members
-        </v-card-title>
-        <v-skeleton-loader type="article">
-        </v-skeleton-loader>
-      </div>
+        <div v-if="getMemberLoaderFlag" :key="'membersLoading'">
+          <v-card-title>
+            Loading Members
+          </v-card-title>
+          <v-skeleton-loader type="article">
+          </v-skeleton-loader>
+        </div>
         <div v-else :key="'membersLoaded'">
-        <v-card-title :key="'volunteerTitle'">
-          Volunteers of {{ getHall | getHallName }} hall
-        </v-card-title>
-
-        <v-card-text>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-              <tr>
-                <th class="text-left">Name</th>
-                <th class="text-left">Batch</th>
-                <th class="text-left">Department</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
+          <v-card-title :key="'volunteerTitle'">
+            Volunteers of {{ getHall | getHallName }} hall
+          </v-card-title>
+          <v-card-subtitle>
+            List is sorted based on activity
+          </v-card-subtitle>
+          <v-card-text>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left">Name</th>
+                  <th class="text-left">Batch</th>
+                  <th class="text-left">Department</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr
                   v-for="(volunteer,index) in getVolunteers"
                   :key="index"
-              >
-                <td>{{ volunteer.name }}</td>
-                <td>{{ volunteer.studentId.substr(0, 2) }}</td>
-                <td>{{ volunteer.studentId | idToDept }}</td>
-              </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-card-text>
-        <v-card-title>
-          Hall Admins of all halls
-        </v-card-title>
-        <v-card-text>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-              <tr>
-                <th class="text-left">
-                  Hall
-                </th>
-                <th class="text-left">
-                  Name
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
+                >
+                  <td>{{ volunteer.name }}<span v-if="index<3"><v-icon style="color: gold">mdi-star</v-icon></span></td>
+                  <td>{{ volunteer.studentId.substr(0, 2) }}</td>
+                  <td>{{ volunteer.studentId | idToDept }}</td>
+                </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
+          <v-card-title>
+            Hall Admins of all halls
+          </v-card-title>
+          <v-card-text>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left">
+                    Hall
+                  </th>
+                  <th class="text-left">
+                    Name
+                  </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr
                   v-for="(hallAdmin,index) in getHallAdmins"
                   :key="index"
-              >
-                <td>{{ hallAdmin.hall | getHallName }}</td>
-                <td>{{ hallAdmin.name }}</td>
-              </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-card-text>
-        <v-card-title>
-          Super Admins
-        </v-card-title>
-        <v-card-text>
-          <v-simple-table>
-            <template v-slot:default>
-              <thead>
-              <tr>
-                <th class="text-left">
-                  Name
-                </th>
-                <th class="text-left">
-                  Phone
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr
+                >
+                  <td>{{ hallAdmin.hall | getHallName }}</td>
+                  <td>{{ hallAdmin.name }}</td>
+                </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
+          <v-card-title>
+            Super Admins
+          </v-card-title>
+          <v-card-text>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left">
+                    Name
+                  </th>
+                  <th class="text-left">
+                    Phone
+                  </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr
                   v-for="(superAdmin,index) in getSuperAdmins"
                   :key="index"
-              >
-                <td>{{ superAdmin.name }}</td>
-                <td>+{{ superAdmin.phone }}</td>
-              </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-card-text>
+                >
+                  <td>{{ superAdmin.name }}</td>
+                  <td>+{{ superAdmin.phone }}</td>
+                </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
         </div>
       </transition>
     </Container>
