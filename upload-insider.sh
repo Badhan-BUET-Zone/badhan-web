61948475bd21e6262687d0b74fbb3742f59bca77
Mir Mahathir Mohammad <36355905+mirmahathir1@users.noreply.github.com>
2023-01-24 23:10:26 +0600

    modified insider script

--- upload-insider.sh
+++ upload-insider.sh
@@ -1,8 +1,8 @@
-# echo "Nano editor will now open. Please edit the version and then save the file"
-# read -n1 -s -r -p $'Press space to continue...\n' key
-# nano ./android/app/build.gradle
+echo "Nano editor will now open. Please edit the version and then save the file"
+read -n1 -s -r -p $'Press space to continue...\n' key
+nano ./android/app/build.gradle
 # nano ./src/plugins/android_support.ts
-npm run build -- --mode insider
-#firebase deploy
-npx cap sync
-# npx cap open android
+bin/run npm run build -- --mode insider
+firebase deploy
+bin/run npx cap sync
+#npx cap open android
